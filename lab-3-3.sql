7920328496b19bb08007c10f67357a8bce602db4
Camila Franca <camila.franca@kellogg.northwestern.edu>
2023-04-06 02:24:10 +0000

    School exercise

--- lab-3-3.sql
+++ lab-3-3.sql
@@ -15,8 +15,9 @@
 -- | 2019 | Houston Astros                | 107       |
 -- | 2004 | St. Louis Cardinals           | 105       |
 
-SELECT year, name, AVG (wins)
+SELECT year, name, MAX(wins)
 FROM teams
-WHERE year > "1960"
+WHERE year >= "1960"
 GROUP BY year
+ORDER BY MAX(wins) DESC
 ;
